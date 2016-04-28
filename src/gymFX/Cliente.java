ef89bfb244c0852e2ffa8c0c17b944daea938486
47993849w <47993849w@iespoblenou.org>
2016-04-28 19:52:54 +0200

    Añadida la logica de crear cliente,

--- src/gymFX/Cliente.java
+++ src/gymFX/Cliente.java
@@ -7,6 +7,23 @@ public class Cliente {
 
     private String nombre;
     private String apellido;
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
     private String apellido2;
     private String dni;
     private String UID;
