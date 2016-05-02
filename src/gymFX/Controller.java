2f193c6f1fc77edd136f0a24ce65146e34c144cb
47993849w <47993849w@iespoblenou.org>
2016-05-02 17:53:27 +0200

    Editar empledo y ver empleado

--- src/gymFX/Controller.java
+++ src/gymFX/Controller.java
@@ -2,6 +2,7 @@ package gymFX;
 
 import com.firebase.client.*;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
@@ -48,6 +49,38 @@ public class Controller {
     public TextField edadVerClienteTF1;
     public TextField numSocioVerClienteTF1;
 
+    public TextField nombreCrearEmpleadoTF;
+    public TextField primerApellidoCrearEmpleadoTF;
+    public TextField segundoApellidoCrearEmpleadoTF;
+    public TextField direccionCrearEmpleadoTF;
+    public TextField emailCrearEmpleadoTF;
+    public TextField telefonoCrearEmpleadoTF;
+    public TextField dniCrearEmpleadoTF;
+    public TextField edadCrearEmpleadoTF;
+    public TextField numEmpeladoCrearEmpleadoTF;
+
+    public TextField nombreVerEmpleadoTF;
+    public TextField primerApellidoVerEmpleadoTF;
+    public TextField segundoApellidoVerEmpleadoTF;
+    public TextField direccionVerEmpleadoTF;
+    public TextField emailVerEmpleadoTF;
+    public TextField telefonoVerEmpleadoTF;
+    public TextField dniVerEmpleadoTF;
+    public TextField edadVerEmpleadoTF;
+    public TextField numEmpeladoVerEmpleadoTF;
+    public Button EditarDatosEmpleado;
+
+    public TextField nombreFichaPropiaTF;
+    public TextField primerApellidoFichaPropiaTF;
+    public TextField segundoApellidoFichaPropiaTF;
+    public TextField direccionFichaPropiaTF;
+    public TextField emailFichaPropiaTF;
+    public TextField telefonoFichaPropiaTF;
+    public TextField dniFichaPropiaTF;
+    public TextField edadFichaPropiaTF;
+    public TextField numEmpeladoFichaPropiaTF;
+
+
     public void initialize(){
         final WebEngine webEngine = webView.getEngine();
         webEngine.load("http://bacderodasport.com");
@@ -61,9 +94,13 @@ public class Controller {
         telefonoVerClienteTF1.setEditable(false);
         edadVerClienteTF1.setEditable(false);
 
-
-
-
+        nombreVerEmpleadoTF.setEditable(false);
+        primerApellidoVerEmpleadoTF.setEditable(false);
+        segundoApellidoVerEmpleadoTF.setEditable(false);
+        direccionVerEmpleadoTF.setEditable(false);
+        emailVerEmpleadoTF.setEditable(false);
+        telefonoVerEmpleadoTF.setEditable(false);
+        edadVerEmpleadoTF.setEditable(false);
     }
 
     public void funLogin(){
@@ -151,7 +188,7 @@ public class Controller {
         });
 
         if (done.get()) {
-            Firebase cref = ref.child("Clientes");
+            Firebase cref = ref.child("Empleados");
             final AtomicBoolean done2 = new AtomicBoolean(false);
 
             Query queryRef = cref.orderByChild("uid").equalTo(userid[0]);
@@ -178,10 +215,21 @@ public class Controller {
             while (!done2.get());
             if (done2.get()){
                 Bienvenido.setText("Bienvenido, "+ empleado.getNombre());
+
+
+
+                nombreFichaPropiaTF.setText(empleado.getNombre());
+                primerApellidoFichaPropiaTF.setText(empleado.getApellido());
+                segundoApellidoFichaPropiaTF.setText(empleado.getApellido2());
+                direccionFichaPropiaTF.setText(empleado.getDireccion());
+                emailFichaPropiaTF.setText(empleado.getEmail());
+                telefonoFichaPropiaTF.setText(empleado.getTelf());
+                dniFichaPropiaTF.setText(empleado.getDni());
+                edadFichaPropiaTF.setText(String.valueOf(empleado.getEdad()));
+                numEmpeladoFichaPropiaTF.setText(empleado.getnEmpleado());
             }
         }
     }
-
 
     public void funCrearClie(){
         Cliente cliente = new Cliente();
@@ -196,7 +244,7 @@ public class Controller {
                 cliente.setNombre(nombreCrearClienteTF.getText());
                 cliente.setApellido(primerApellidoCrearClienteTF.getText());
                 cliente.setApellido2(segundoApellidoCrearClienteTF.getText());
-                cliente.setDni(dniCrearClienteTF.getText()+"4");
+                cliente.setDni(dniCrearClienteTF.getText());
                 cliente.setDireccion(direccionCrearCLienteTF.getText());
                 cliente.setEdad(Integer.parseInt(edadCrearClienteTF.getText()));
                 cliente.setEmail(emailCrearClienteTF.getText());
@@ -342,14 +390,12 @@ public class Controller {
             Map<String, Object> cl = new HashMap<String, Object>();
             cl.put("apellido", primerApellidoVerClienteTF1.getText());
             cl.put("apellido2", segundoApellidoVerClienteTF1.getText());
-            cl.put("nombre", primerApellidoVerClienteTF1.getText());
+            cl.put("nombre", nombreVerClienteTF1.getText());
             cl.put("edad", edadVerClienteTF1.getText());
             cl.put("direccion", direccionVerClienteTF1.getText());
             cl.put("email", emailVerClienteTF1.getText());
             cl.put("dni", dniVerClienteTF1.getText());
-
             cl.put("telf", telefonoVerClienteTF1.getText());
-            cl.put("apellido", primerApellidoVerClienteTF1.getText());
 
 
 
@@ -364,6 +410,191 @@ public class Controller {
             telefonoVerClienteTF1.setEditable(false);
             edadVerClienteTF1.setEditable(false);
             EditarDatosCliente.setText("Editar datos");
+        }
+    }
+
+    public void funBuscarEmple(){
+
+        AtomicBoolean hechoBCNS = new AtomicBoolean(false);
+        errorBusqueda = false;
+        Firebase ref = new Firebase("https://fiery-inferno-9835.firebaseio.com/");
+        Firebase cref = ref.child("Empleados");
+        Query queryRef = cref.orderByChild("nEmpleado").equalTo(numEmpeladoVerEmpleadoTF.getText());
+        queryRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue()==null){
+                    errorBusqueda = true;
+                    hechoBCNS.set(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+                snapshot1=snapshot;                                                                             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+                Empleado empleado = snapshot.getValue(Empleado.class);
+                nombreVerEmpleadoTF.setText(empleado.getNombre());
+                primerApellidoVerEmpleadoTF.setText(empleado.getApellido());
+                segundoApellidoVerEmpleadoTF.setText(empleado.getApellido2());
+                direccionVerEmpleadoTF.setText(empleado.getDireccion());
+                emailVerEmpleadoTF.setText(empleado.getEmail());
+                dniVerEmpleadoTF.setText(empleado.getDni());
+                telefonoVerEmpleadoTF.setText(empleado.getTelf());
+                edadVerEmpleadoTF.setText(String.valueOf(empleado.getEdad()));
+                hechoBCNS.set(true);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+        while (!hechoBCNS.get());
+        if (errorBusqueda){
+            alertError.setTitle("Error Busqueda Num Empleado");
+            alertError.setHeaderText(null);
+            alertError.setContentText("No hay ningun empleado con este Numero de Empleado!");
+            alertError.showAndWait();
+        }
+    }
+
+    public void funBuscarEmpleDNI(){
+        AtomicBoolean hechoBCDNI = new AtomicBoolean(false);
+        errorBusqueda = false;
+        Firebase ref = new Firebase("https://fiery-inferno-9835.firebaseio.com/");
+        Firebase cref = ref.child("Empleados");
+        Query queryRef = cref.orderByChild("dni").equalTo(dniVerEmpleadoTF.getText());
+        queryRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue()==null){
+                    errorBusqueda = true;
+                    hechoBCDNI.set(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+                snapshot1=snapshot;
+                Empleado empleado = snapshot.getValue(Empleado.class);
+                nombreVerEmpleadoTF.setText(empleado.getNombre());
+                primerApellidoVerEmpleadoTF.setText(empleado.getApellido());
+                segundoApellidoVerEmpleadoTF.setText(empleado.getApellido2());
+                direccionVerEmpleadoTF.setText(empleado.getDireccion());
+                emailVerEmpleadoTF.setText(empleado.getEmail());
+                telefonoVerEmpleadoTF.setText(empleado.getTelf());
+                edadVerEmpleadoTF.setText(String.valueOf(empleado.getEdad()));
+                numEmpeladoVerEmpleadoTF.setText(empleado.getnEmpleado());
+                hechoBCDNI.set(true);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+        while (!hechoBCDNI.get());
+        if (errorBusqueda){
+            alertError.setTitle("Error Busqueda DNI");
+            alertError.setHeaderText(null);
+            alertError.setContentText("No hay ningun Empleado con este DNI!");
+            alertError.showAndWait();
+        }
+    }
+
+    public void funCrearEmple() {
+        Empleado empleado = new Empleado();
+        Firebase ref = new Firebase("https://fiery-inferno-9835.firebaseio.com/");
+        Firebase cref = ref.child("Empleados");
+        Firebase.ValueResultHandler<Map<String, Object>> handler = new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                System.out.println("Successfully created user account with uid: " + result.get("uid"));
+                String a = (String) result.get("uid");
+                empleado.setUID(a);
+                empleado.setNombre(nombreCrearEmpleadoTF.getText());
+                empleado.setApellido(primerApellidoCrearEmpleadoTF.getText());
+                empleado.setApellido2(segundoApellidoCrearEmpleadoTF.getText());
+                empleado.setDni(dniCrearEmpleadoTF.getText());
+                empleado.setDireccion(direccionCrearEmpleadoTF.getText());
+                empleado.setEdad(Integer.parseInt(edadCrearEmpleadoTF.getText()));
+                empleado.setEmail(emailCrearEmpleadoTF.getText());
+                empleado.setTelf(telefonoCrearEmpleadoTF.getText());
+                empleado.setnEmpleado(numEmpeladoCrearEmpleadoTF.getText());
+                cref.push().setValue(empleado);
+            }
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                System.out.println(firebaseError);
+            }
+        };
+        ref.createUser(emailCrearEmpleadoTF.getText(),"test1", handler);
+
+
+
+    }
+
+    public void funEditEmple(){
+        if (EditarDatosEmpleado.getText().equals("Editar datos")) {
+            nombreVerEmpleadoTF.setEditable(true);
+            primerApellidoVerEmpleadoTF.setEditable(true);
+            segundoApellidoVerEmpleadoTF.setEditable(true);
+            direccionVerEmpleadoTF.setEditable(true);
+            emailVerEmpleadoTF.setEditable(true);
+            telefonoVerEmpleadoTF.setEditable(true);
+            edadVerEmpleadoTF.setEditable(true);
+            EditarDatosEmpleado.setText("Aplicar Cambios");
+
+        } else {
+            Firebase ref = new Firebase("https://fiery-inferno-9835.firebaseio.com/");
+            Firebase cref = ref.child("Empleados");
+            Firebase editRef = cref.child(snapshot1.getKey());
+            Map<String, Object> cl = new HashMap<String, Object>();
+            cl.put("apellido", primerApellidoVerEmpleadoTF.getText());
+            cl.put("apellido2", segundoApellidoVerEmpleadoTF.getText());
+            cl.put("nombre", nombreVerEmpleadoTF.getText());
+            cl.put("edad", edadVerEmpleadoTF.getText());
+            cl.put("direccion", direccionVerEmpleadoTF.getText());
+            cl.put("email", emailVerEmpleadoTF.getText());
+            cl.put("dni", dniVerEmpleadoTF.getText());
+            cl.put("telf", telefonoVerEmpleadoTF.getText());
+
+
+
+
+            editRef.updateChildren(cl);
+            funBuscarEmple();
+            nombreVerEmpleadoTF.setEditable(false);
+            primerApellidoVerEmpleadoTF.setEditable(false);
+            segundoApellidoVerEmpleadoTF.setEditable(false);
+            direccionVerEmpleadoTF.setEditable(false);
+            emailVerEmpleadoTF.setEditable(false);
+            telefonoVerEmpleadoTF.setEditable(false);
+            edadVerEmpleadoTF.setEditable(false);
+            EditarDatosEmpleado.setText("Editar datos");
         }
     }
 }
