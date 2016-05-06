8829b4a1f78ee09db23094ddaa6d9c4f08119de1
oscar <46996403c@iespoblenou.org>
2016-05-06 20:20:29 +0200

    Cambio tamaño ventana

--- src/gymFX/Main.java
+++ src/gymFX/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1600, 900));
+        primaryStage.setScene(new Scene(root, 1060, 704));
         primaryStage.show();
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
